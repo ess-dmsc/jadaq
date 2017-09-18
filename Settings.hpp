8d001f326c256ef3d0bd670d1b0418973dc94b01
Troels Blum <troels@blum.dk>
2017-09-18 15:10:50 +0200

    Implemented calling getters

--- Settings.hpp
+++ Settings.hpp
@@ -44,4 +44,7 @@ enum FunctionID {
 
 void set(caen::Digitizer* digitizer, FunctionID functionID, std::string value);
 void set(caen::Digitizer* digitizer, FunctionID functionID, int index, std::string value);
+std::string get(caen::Digitizer* digitizer, FunctionID functionID);
+std::string get(caen::Digitizer* digitizer, FunctionID functionID, int index);
+
 #endif //JADAQ_SETTINGS_HPP
