e1d16fa21aea4acbc1c8829faac1eecb22ed08ad
Troels Blum <troels@blum.dk>
2018-04-19 09:34:51 +0200

    Implemented NULL data handler

---
+++ DataHandlerNull.hpp
@@ -0,0 +1,39 @@
+/**
+ * jadaq (Just Another DAQ)
+ * Copyright (C) 2018  Troels Blum <troels@blum.dk>
+ *
+ * @author Troels Blum <troels@blum.dk>
+ * @section LICENSE
+ * This program is free software: you can redistribute it and/or modify
+ *        it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ *         but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @section DESCRIPTION
+ * Data handler that simply throws data away - for performance testing purposes
+ *
+ */
+
+#ifndef JADAQ_DATAHANDLERNULL_HPP
+#define JADAQ_DATAHANDLERNULL_HPP
+
+#include "DataHandler.hpp"
+
+class DataHandlerNull: public DataHandler
+{
+public:
+    void initialize(uuid runID, uint32_t digitizerID) override {}
+    void addEvent(Data::ListElement422 event) override {}
+    void tick(uint64_t timeStamp) override {}
+};
+
+
+#endif //JADAQ_DATAHANDLERNULL_HPP
