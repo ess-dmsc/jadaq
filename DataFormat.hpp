e59520d58a2109480d94a36414dfd6beba989063
Troels Blum <troels@blum.dk>
2018-05-17 09:38:53 +0200

    Packing datastructures for network

--- DataFormat.hpp
+++ DataFormat.hpp
@@ -46,7 +46,7 @@ namespace Data
         Waweform
     };
     /* Shared meta data for the entire data package */
-    struct Header // 32 bytes
+    struct __attribute__ ((__packed__)) Header // 32 bytes
     {
         uint64_t runID;
         uint64_t globalTime;
@@ -56,7 +56,7 @@ namespace Data
         uint16_t version;
         uint8_t __pad[6];
     };
-    struct ListElement422
+    struct __attribute__ ((__packed__)) ListElement422
     {
         uint32_t localTime;
         uint16_t adcValue;
@@ -97,7 +97,7 @@ namespace Data
 
 } // namespace Data
 
-//TODO: template for all Data::Element types
 static inline std::ostream& operator<< (std::ostream& os, const Data::ListElement422& e)
 { e.printOn(os); return os; }
+
 #endif //JADAQ_DATAFORMAT_HPP
