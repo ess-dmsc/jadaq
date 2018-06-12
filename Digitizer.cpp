54c13cfb32947ed4e9e1cb4aa0e80282a2c18fc8
Troels Blum <troels@blum.dk>
2018-06-12 17:51:33 +0200

    better errormessages

--- Digitizer.cpp
+++ Digitizer.cpp
@@ -263,12 +263,32 @@ static R backOffRepeat(F fun, int retry=3,  std::chrono::milliseconds grace=std:
 
 std::string Digitizer::get(FunctionID functionID, int index)
 {
-    return backOffRepeat<std::string>([this,&functionID,&index](){ return get_(digitizer,functionID,index); });
+    std::string res;
+    try
+    {
+        res = backOffRepeat<std::string>([this,&functionID,&index](){ return get_(digitizer,functionID,index); });
+    }
+    catch (...)
+    {
+        std::cerr << "ERROR: calling get" << to_string(id) << "(" << index << ")" << std::endl;
+        throw;
+    }
+    return res;
 }
 
 std::string Digitizer::get(FunctionID functionID)
 {
-    return backOffRepeat<std::string>([this,&functionID](){ return get_(digitizer,functionID); });
+    std::string res;
+    try
+    {
+        res = backOffRepeat<std::string>([this, &functionID]() { return get_(digitizer, functionID); });
+    }
+    catch (...)
+    {
+        std::cerr << "ERROR: calling get" << to_string(id) << "()" << std::endl;
+        throw;
+    }
+    return res;
 }
 
 void Digitizer::set(FunctionID functionID, int index, std::string value)
@@ -276,10 +296,19 @@ void Digitizer::set(FunctionID functionID, int index, std::string value)
     try
     {
         backOffRepeat<void>([this,&functionID,&index,&value](){ return set_(digitizer,functionID,index,value); });
-    } catch (std::invalid_argument& e)
+    }
+    catch (std::invalid_argument& e)
     {
         if(functionID != Register)
+        {
+            std::cerr << "ERROR: calling set" << to_string(id) << "(" << index << ", " << value << ")" << std::endl;
             throw;
+        }
+    }
+    catch (...)
+    {
+        std::cerr << "ERROR: calling set" << to_string(id) << "(" << index << ", " << value << ")" << std::endl;
+        throw;
     }
     if (functionID == Register)
     {
@@ -289,7 +318,16 @@ void Digitizer::set(FunctionID functionID, int index, std::string value)
 
 void Digitizer::set(FunctionID functionID, std::string value)
 {
-    return backOffRepeat<void>([this,&functionID,&value](){ return set_(digitizer,functionID,value); });
+    try
+    {
+        backOffRepeat<void>([this, &functionID, &value]() { return set_(digitizer, functionID, value); });
+    }
+    catch (...)
+    {
+        std::cerr << "ERROR: calling set" << to_string(id) << "(" << value << ")" << std::endl;
+        throw;
+    }
+
 }
 
 Digitizer::Digitizer(CAEN_DGTZ_ConnectionType linkType_, int linkNum_, int conetNode_, uint32_t VMEBaseAddress_)
