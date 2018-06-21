6870af7d95956759a1d310f112abe21905b535c2
Troels Blum <troels@blum.dk>
2018-06-21 16:45:16 +0200

    bugfix

--- EventIterator.hpp
+++ EventIterator.hpp
@@ -166,6 +166,7 @@ public:
     {
         if (++groupIterator == boardAggregateEnd)
         {
+            ptr = boardAggregateEnd;
             groupIterator = nextGroupIterator();
         }
         return *this;
